d948f1d9ff7593499032647ddc25dc117bcd6504
BugCatcherNate <natetthompson@gmail.com>
2021-05-23 14:22:30 -0500

    added glad

--- main.cpp
+++ main.cpp
@@ -5,8 +5,17 @@
 #include <glm/glm.hpp>
 #include <vector>
 #include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 using namespace std;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 int main(int, char **)
 {
